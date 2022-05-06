5429d769f7a14340ffd9358967006becd58c3501
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2022-05-06 18:18:08 +0100

    input chnages

--- components/Input.jsx
+++ components/Input.jsx
@@ -12,14 +12,14 @@ const Input = () => {
     const items = useSelector(selectItems)
     const dispatch = useDispatch();
 
-    // useEffect((input) => {
-    //     if(input === ''){
-    //         setFormInput(false)
-    //     }else{
-    //         setFormInput(true)
-    //     }
+    useEffect(() => {
+        if(input === ''){
+            setFormInput(true)
+        }else{
+            setFormInput(false)
+        }
         
-    // }, [input])
+    }, [input])
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -31,9 +31,12 @@ const Input = () => {
     var h = d.getHours()
     var s = d.getSeconds()
     d = h + ':' + mins + ':'+ s;
+
+
    
         
-    const addToTodo = () => {
+    const addToTodo = (e) => {
+      e.preventDefault()
        dispatch(addtoTodo({
            item: input,
            done: false,
@@ -41,14 +44,15 @@ const Input = () => {
            min: today
        }));
        //console.log(input)
+       setInput('')
     }
 
   return (
         <section className='md:mx-auto px-4  md:w-[50%] mt-[100px]'>
-            <form action="" >
+            <form action="" onSubmit={addToTodo}>
                  <div className='flex w-full border border-red-700 rounded-xl overflow-hidden'>
-                    <input className='flex-grow py-2 px-4 md:text-2xl outline-none' value={(input)} type="text" placeholder='Type Here' onChange={(e) => setInput(e.target.value)}/>
-                    <div  onClick={addToTodo} className={`bg-red-800 py-2 px-4 md:py-3 md:px-8 text-white cursor-pointer`}>Add</div>
+                    <input className='flex-grow py-2 px-4 md:text-2xl outline-none' value={input} type="text" placeholder='Type Here' onChange={(e) => setInput(e.target.value)}/>
+                    <button disabled={formInput} type='submit' className={`bg-red-800 py-2 px-4 md:py-3 md:px-8 text-white cursor-pointer`}>Add</button>
                 </div>
                 <div>{items.length}</div>
             </form>
